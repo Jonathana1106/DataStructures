a36bae1472ba9fda0c8a1f983a829c81debf8899
Jonathana1196 <jonathana1196@gmail.com>
2019-04-17 23:02:36 -0600

    Pruebas incompletas

--- src/com/joty/sorter/LauncherSort.java
+++ src/com/joty/sorter/LauncherSort.java
@@ -4,131 +4,125 @@
 package com.joty.sorter;
 
 /**
- * @author Jonathan
+ * Clase que ejecuta las pruebas de los diferentes algoritmos.
+ * 
+ * @author Jonathan G Araya
  *
  */
 public class LauncherSort {
 
-    /**
-     * @param <T>
-     * @param args
-     */
-    public static <T> void main(String[] args) {
+	/**
+	 * @param <T>
+	 * @param args
+	 */
+	public static <T> void main(String[] args) {
 
-	// BubbleSort burbuja = new BubbleSort();
-	//
-	// int arreglo[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	// System.out.println("Arreglo original");
-	// burbuja.printL(arreglo);
-	// System.out.println();
-	// burbuja.bubbleSort(arreglo);
-	// burbuja.printL(arreglo);
+//		 BubbleSort burbuja = new BubbleSort();
+//		
+//		 int arreglo[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		 System.out.println("Arreglo original");
+//		 burbuja.printL(arreglo);
+//		 System.out.println();
+//		 burbuja.bubbleSort(arreglo);
+//		 burbuja.printL(arreglo);
 
-	// RadixSort radix = new RadixSort();
-	//
-	// int arreglo[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-	// System.out.println("Arreglo Original");
-	// radix.printL(arreglo);
-	// radix.radixSort(arreglo);
+//		RadixSort radix = new RadixSort();
+//
+//		int arreglo[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+//		System.out.println("Arreglo Original");
+//		radix.printL(arreglo);
+//		radix.radixSort(arreglo);
 
-	// RadixPrueba radix = new RadixPrueba();
-	//
-	// String arreglo[] = { "z", "r", "d", "h", "l", "k", "p", "s", "c", "t", "b" };
-	// System.out.println("Arreglo original");
-	// radix.printL(arreglo);
-	// System.out.println();
-	// radix.radixSort(arreglo);
-	// System.out.println("Arreglo Ordenado");
-	// radix.printL(arreglo);
+//		QuickSort quick = new QuickSort();
+//
+//		int arreglo[] = { 8, 3, 6, 4, 2, 5, 7, 1 };
+//		System.out.println("Arreglo original");
+//		quick.printL(arreglo);
+//		System.out.println("Arreglo ordenado");
+//		quick.quickSort(arreglo);
+//		quick.printL(arreglo);
 
-	// QuickSort quick = new QuickSort();
-	//
-	// String arreglo[] = { "z", "r", "d", "h", "l", "k", "p", "s", "c", "t", "b" };
-	// System.out.println("Arreglo original");
-	// quick.printL(arreglo);
-	// System.out.println("Arreglo ordenado");
-	// quick.quickSort(arreglo);
-	// quick.printL(arreglo);
+//		 ShellSort shell = new ShellSort();
+//		
+//		 int arreglo[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		 System.out.println("Arreglo Original");
+//		 shell.printL(arreglo);
+//		 System.out.println();
+//		 shell.shellSort(arreglo);
+//		 System.out.println("Arreglo Ordenado");
+//		 shell.printL(arreglo);
 
-	// ShellSort shell = new ShellSort();
-	//
-	// int arreglo[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	// System.out.println("Arreglo Original");
-	// shell.printL(arreglo);
-	// System.out.println();
-	// shell.shellSort(arreglo);
-	// System.out.println("Arreglo Ordenado");
-	// shell.printL(arreglo);
-	//
-	// BinarySearch i = new BinarySearch();
-	//
-	// i.binarySeach(arreglo, 7);
+		// BinarySearch i = new BinarySearch();
+		//
+		// i.binarySeach(arreglo, 7);
 
-	// GenericBubbleSort bubble = new GenericBubbleSort<>();
-	//
-	// SimpleLL arreglo = new SimpleLL<>();
-	//
-	// arreglo.add("y");
-	// arreglo.add("v");
-	// arreglo.add("c");
-	// arreglo.add("p");
-	// arreglo.add("l");
-	// arreglo.add("u");
-	// arreglo.add("k");
-	// arreglo.add("s");
-	// arreglo.add("m");
-	// arreglo.add("d");
-	// arreglo.add("z");
-	// System.out.println("Arreglo Original");
-	// bubble.printL(arreglo);
-	// arreglo.deleteValue(2);
-	// System.out.println();
-	// bubble.bubbleSort(arreglo);
-	// System.out.println("Arreglo Ordenado");
-	// bubble.printL(arreglo);
+		// GenericBubbleSort bubble = new GenericBubbleSort<>();
+		//
+		// SimpleLL arreglo = new SimpleLL<>();
+		//
+		// arreglo.add("y");
+		// arreglo.add("v");
+		// arreglo.add("c");
+		// arreglo.add("p");
+		// arreglo.add("l");
+		// arreglo.add("u");
+		// arreglo.add("k");
+		// arreglo.add("s");
+		// arreglo.add("m");
+		// arreglo.add("d");
+		// arreglo.add("z");
+		// System.out.println("Arreglo Original");
+		// bubble.printL(arreglo);
+		// arreglo.deleteValue(2);
+		// System.out.println();
+		// bubble.bubbleSort(arreglo);
+		// System.out.println("Arreglo Ordenado");
+		// bubble.printL(arreglo);
 
-	// GenericQuickSort quick = new GenericQuickSort<>();
-	//
-	// SimpleLL arreglo = new SimpleLL<>();
-	//
-	// arreglo.add("h");
-	// arreglo.add("t");
-	// arreglo.add("m");
-	// arreglo.add("m");
-	// arreglo.add("o");
-	// arreglo.add("z");
-	// arreglo.add("a");
-	// arreglo.add("k");
-	// arreglo.add("b");
-	// arreglo.add("v");
-	// arreglo.add("e");
-	// System.out.println();
-	// System.out.println("El tamano del arreglo es: " + arreglo.getSize());
-	// System.out.println();
-	// System.out.println("Arreglo Original");
-	// quick.printL(arreglo);
-	// System.out.println();
-	// quick.quickSort(arreglo);
-	// System.out.println("Arreglo Ordenado");
-	// quick.printL(arreglo);
+//		GenericQuickSort quick = new GenericQuickSort<>();
+//
+//		SimpleLL arreglo = new SimpleLL<>();
+//
+//		arreglo.add("h");
+//		arreglo.add("t");
+//		arreglo.add("m");
+//		arreglo.add("m");
+//		arreglo.add("o");
+//		arreglo.add("z");
+//		arreglo.add("a");
+//		arreglo.add("k");
+//		arreglo.add("b");
+//		arreglo.add("v");
+//		arreglo.add("e");
+//		System.out.println();
+//		System.out.println("El tamano del arreglo es: " + arreglo.getSize());
+//		System.out.println();
+//		System.out.println("Arreglo Original");
+//		quick.printL(arreglo);
+//		System.out.println();
+//		quick.quickSort(arreglo);
+//		System.out.println("Arreglo Ordenado");
+//		quick.printL(arreglo);
 
-	// BinarySearch binary = new BinarySearch();
-	// SecuencialSearch secuencial = new SecuencialSearch();
-	//
-	// int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	// int data = 8;
-	//
-	// binary.binarySearch(array, data);
-	// secuencial.secuencialSearch(array, data);
+//		BinarySearch binary = new BinarySearch();
+//		 SecuencialSearch secuencial = new SecuencialSearch();
+//		
+//		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//		int data = 8;
+//		
+//		binary.binarySearch(array, data);
+//		 secuencial.secuencialSearch(array, data);
 
-	// GenericSecuencialSearch secuencial = new GenericSecuencialSearch<>();
-	// Comparable[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	// int data = 6;
-	// String array2[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-	// "l" };
-	// String data2 = "k";
-	//
-	// secuencial.genericSecuencialSearch(array, data);
-	// secuencial.genericSecuencialSearch(array2, data2);
-    }
+//		GenericBinarySearch binarys = new GenericBinarySearch<>();
+//		GenericSecuencialSearch secuencial = new GenericSecuencialSearch<>();
+//		Comparable[] arrays = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//		int datas = 6;
+//		String array2[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" };
+//		String data2 = "k";
+//
+//		binarys.binarySearch(arrays, datas);
+//		secuencial.genericSecuencialSearch(array, data);
+//		secuencial.genericSecuencialSearch(array2, data2);
+
+	}
 }
