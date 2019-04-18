b362d85fa885c7a9eb6d92b94ab07b40064eeae6
Jonathana1196 <jonathana1196@gmail.com>
2019-04-17 22:39:24 -0600

    Final

--- src/com/joty/sorter/GenericSecuencialSearch.java
+++ src/com/joty/sorter/GenericSecuencialSearch.java
@@ -1,31 +1,41 @@
 /**
- * 
+ * Clase generica que se encarga de realizar una búqueda secuencial.
  */
 package com.joty.sorter;
 
 /**
- * @author Jonathan
+ * @author Jonathan G Araya
  * @param <T>
  *
  */
 public class GenericSecuencialSearch<T extends Comparable<T>> {
 
-    public GenericSecuencialSearch() {
-    }
-
-    public T genericSecuencialSearch(T array[], T data) {
-	int i = 0;
-
-	while (i < array.length) {
-
-	    if (array[i] == data) {
-		System.out.println("The searched element it's in the index: " + i);
-		return data;
-	    } else {
-		i++;
-	    }
+	/**
+	 * Método que se encarga de inicializar la clase.
+	 */
+	public GenericSecuencialSearch() {
 	}
-	System.out.println("The searched element was not found.");
-	return data;
-    }
+
+	/**
+	 * Método que se encarga de realizar una búsqueda secuencial.
+	 * 
+	 * @param array sobre el cuál se va a realizar la búsqueda.
+	 * @param data  a buscar.
+	 * @return el dato encontrado.
+	 */
+	public T genericSecuencialSearch(T array[], T data) {
+		int i = 0;
+
+		while (i < array.length) {
+
+			if (array[i] == data) {
+				System.out.println("The searched element it's in the index: " + i);
+				return data;
+			} else {
+				i++;
+			}
+		}
+		System.out.println("The searched element was not found.");
+		return data;
+	}
 }
