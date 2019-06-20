5014a42d20903971fb53e5415c6e27338024d4d1
Jonathan G Araya <jonathana1196@gmail.com>
2019-06-20 02:29:19 -0600

    Launcher

--- src/com/joty/listas/LauncherListas.java
+++ src/com/joty/listas/LauncherListas.java
@@ -15,33 +15,33 @@ public class LauncherListas {
 	 */
 	public static <T> void main(String[] args) {
 
-		SimpleLL array = new SimpleLL<T>();
-
-		array.add(1);
-		array.add(0);
-		array.add(9);
-		array.add(8);
-		array.add(7);
-		array.add(6);
-		array.add(5);
-		array.add(4);
-		array.add(3);
-		array.add(2);
-
-		array.show();
-
-		array.getSize();
-
-		array.getDataIndex(6);
-
-		array.deleteValue(4);
-
-		array.show();
-
-		array.getSize();
-
-		array.getDataIndex(6);
+//		@SuppressWarnings("rawtypes")
+//		SimpleLL array = new SimpleLL<T>();
+//
+//		array.add(1);
+//		array.add(0);
+//		array.add(9);
+//		array.add(8);
+//		array.add(7);
+//		array.add(6);
+//		array.add(5);
+//		array.add(4);
+//		array.add(3);
+//		array.add(2);
+//
+//		array.show();
+//
+//		array.getSize();
+//
+//		array.getDataIndex(6);
+//
+//		array.deleteValue(4);
+//
+//		array.show();
+//
+//		array.getSize();
+//
+//		array.getDataIndex(6);
 
 	}
-
 }
